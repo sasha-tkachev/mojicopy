6ae0bff82a148f31f40b4d5890c83ce5a7ed2f3f
Alexander Tkachev <sasha64sasha@gmail.com>
2023-01-14 21:16:19 +0200

    feat: add mojicopy entrypoint

--- setup.py
+++ setup.py
@@ -41,4 +41,9 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
         ],
+        entry_points={
+            "console_scripts": [
+                "mojicopy = mojicopy.cli:app",
+            ],
+        },
     )
