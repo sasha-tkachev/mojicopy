4eb2c76f8c33a135b94bf53ac8d1ff591dd4c95d
Alexander Tkachev <sasha64sasha@gmail.com>
2023-01-14 17:26:14 +0200

    refactor: remove uneeded prints

--- mojicopy/ai_emoji.py
+++ mojicopy/ai_emoji.py
@@ -4,13 +4,11 @@ from mojicopy.settings import OpenAiSettings
 
 
 def ai_emoji(prompt: str, settings: OpenAiSettings) -> str:
-    print("Here")
     response = openai.Completion.create(
         model=settings.model,
         prompt=f"""Give me a single emoji that describes "{prompt}"?""",
         max_tokens=settings.max_tokens,
     )
-    print("got response")
     if response.choices:
         return response.choices[0].text.strip()
     raise RuntimeError("TODO: multiple attempts")
