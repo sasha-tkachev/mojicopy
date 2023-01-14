cdbb6a5b06f2d0a1f0b2adbf583855c4e662bdd4
Alexander Tkachev <sasha64sasha@gmail.com>
2023-01-14 17:25:49 +0200

    feat: output emoji

--- mojicopy/cli.py
+++ mojicopy/cli.py
@@ -18,18 +18,30 @@ def _query_prompt() -> Optional[str]:
         return None
 
 
-def _select_emoji(random: bool, settings: Settings) -> str:
+def _select_emoji(
+    random: bool, settings: Settings, prompt: Optional[str] = None
+) -> str:
     if random:
         return random_emoji()
     else:
-        prompt = _query_prompt()
+        if prompt is None:
+            prompt = _query_prompt()
         if prompt:
-            return ai_emoji(xerox.paste(), settings.openai)
+            return ai_emoji(prompt, settings.openai)
         return random_emoji()
 
 
+def _output_emoji(emoji: str) -> None:
+    typer.echo(emoji)
+    xerox.copy(emoji)
+
+
 @app.command()
-def mojicopy(random: bool = False):
+def mojicopy(random: bool = False, prompt: Optional[str] = None):
+    if random:
+        if prompt is not None:
+            typer.echo("Can't generate random ai emoji from prompt", err=True)
+            exit(1)
     settings = Settings()
     if not random:
         if settings.openai.api_key is None:
@@ -43,9 +55,7 @@ def mojicopy(random: bool = False):
             persist_settings(settings)
         openai.api_key = settings.openai.api_key
 
-    emoji = _select_emoji(random, settings)
-    typer.echo(emoji)
-    xerox.copy(emoji)
+    _output_emoji(_select_emoji(random, settings, prompt=prompt))
 
 
 if __name__ == "__main__":
